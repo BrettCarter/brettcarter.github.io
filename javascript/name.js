8d2b6974fc1b30fcbf1aa4bf8ef0420e289e7485
BrettCarter <brettcartersud@gmail.com>
2015-06-12 09:03:49 -0600

    Initial Commit

---
+++ javascript/name.js
@@ -0,0 +1,14 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [80, 50, 50];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Brett";
+// var letterColors = [red, orange, green, blue, purple];
+var letterColors = [green];
+
+bubbleShape = "square";
+
+drawName(myName, letterColors);
+bounceBubbles();
